9acce9bccd54379664ba9e6942f135048939ba51
rulanugrh <nextarituenak01@gmail.com>
2024-03-24 17:52:48 +0700

    chore: adding script for install require or build

--- build.sh
+++ build.sh
@@ -1,3 +1,34 @@
 #!/usr/bin/env bash
 
-echo "Hello World"
+command=$1
+
+check_os() {
+    . /etc/os-release
+    case $ID in
+        ubuntu)
+        sudo apt install wget curl docker docker.io -y
+        ;;
+        arch)
+        sudo pacman -S docker wget curl
+        ;;
+        debian)
+        sudo apt install wget curl docker docker.io -y
+        ;;
+        centos)
+        sudo yum install -y yum-utils && sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo && sudo yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+        ;;
+    esac
+}
+
+if [[ $command == "requirements" ]]
+then
+    check_os
+    exit 0
+else if [[ $command == "build" ]]
+then
+    docker compose up -d db-product && docker compose up -d db-user && docker compose up -d
+    exit
+else
+    echo "sorry invalid command"
+fi
+    

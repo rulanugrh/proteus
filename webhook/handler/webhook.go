01c4f57d5762a6d59582a83dd5a68a6c4fec0396
rulanugrh <nextarituenak01@gmail.com>
2024-03-31 16:02:36 +0700

    chore: adding webhook for VA and capture

--- webhook/handler/webhook.go
+++ webhook/handler/webhook.go
@@ -30,6 +30,78 @@ func NewWebhookHandler(rabbit helper.IRabbitMQ) *WebhookHandler {
 	return &WebhookHandler{rabbit: rabbit}
 }
 
+func (wh *WebhookHandler) FVAPaid(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Println("[x] Error read body")
+		w.WriteHeader(400)
+		w.Write(helper.Marshal(helper.BadRequest(err.Error(), "bad request")))
+		return
+	}
+	log.Println("[*] Webhook FVA Paid")
+	log.Println("[*] Response")
+	log.Println(string(body))
+
+	response := helper.Marshal(helper.Success("fva paid", "ok", nil))
+	w.WriteHeader(200)
+	w.Write(response)
+	return
+}
+
+func (wh *WebhookHandler) FVACreated(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Println("[x] Error read body")
+		w.WriteHeader(400)
+		w.Write(helper.Marshal(helper.BadRequest(err.Error(), "bad request")))
+		return
+	}
+	log.Println("[*] Webhook FVA Created")
+	log.Println("[*] Response")
+	log.Println(string(body))
+
+	response := helper.Marshal(helper.Success("fva created", "ok", nil))
+	w.WriteHeader(200)
+	w.Write(response)
+	return
+}
+
+func (wh *WebhookHandler) CaptureSuccess(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Println("[x] Error read body")
+		w.WriteHeader(400)
+		w.Write(helper.Marshal(helper.BadRequest(err.Error(), "bad request")))
+		return
+	}
+	log.Println("[*] Webhook Capture Success")
+	log.Println("[*] Response")
+	log.Println(string(body))
+
+	response := helper.Marshal(helper.Success("capture success", "ok", nil))
+	w.WriteHeader(200)
+	w.Write(response)
+	return
+}
+
+func (wh *WebhookHandler) CaptureFailed(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Println("[x] Error read body")
+		w.WriteHeader(400)
+		w.Write(helper.Marshal(helper.BadRequest(err.Error(), "bad request")))
+		return
+	}
+	log.Println("[*] Webhook Capture Failed")
+	log.Println("[*] Response")
+	log.Println(string(body))
+
+	response := helper.Marshal(helper.Success("capture failed", "ok", nil))
+	w.WriteHeader(200)
+	w.Write(response)
+	return
+}
+
 func (wh *WebhookHandler) PaymentSucess(w http.ResponseWriter, r *http.Request) {
 
 	body, err := ioutil.ReadAll(r.Body)
--- webhook/routes/route.go
+++ webhook/routes/route.go
@@ -12,6 +12,10 @@ func HandlerRoute(webhook *handler.WebhookHandler, addr string) error {
 	serv.HandleFunc("/payment_failed", webhook.PaymentFailed)
 	serv.HandleFunc("/payment_waiting", webhook.AwaitPaymentCapture)
 	serv.HandleFunc("/payment_failed", webhook.PaymentFailed)
+	serv.HandleFunc("/va_created", webhook.FVACreated)
+	serv.HandleFunc("/va_paid", webhook.FVAPaid)
+	serv.HandleFunc("/capture_success", webhook.CaptureSuccess)
+	serv.HandleFunc("/capture_failed", webhook.CaptureFailed)
 
 	server := http.Server{
 		Addr: addr,

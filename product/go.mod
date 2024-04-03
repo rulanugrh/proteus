2e35ddc8c7a5d04c9631ce687d6954ee39d954ea
rulanugrh <nextarituenak01@gmail.com>
2024-04-03 21:26:24 +0700

    feat: mockup repository for product services

--- product/go.mod
+++ product/go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/prometheus/client_golang v1.19.0
 	github.com/rabbitmq/amqp091-go v1.9.0
 	github.com/sirupsen/logrus v1.9.3
+	github.com/stretchr/testify v1.9.0
 	gorm.io/driver/postgres v1.5.7
 	gorm.io/gorm v1.25.8
 )
@@ -16,16 +17,20 @@ require (
 require (
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/pgx/v5 v5.4.3 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/prometheus/client_model v0.5.0 // indirect
 	github.com/prometheus/common v0.48.0 // indirect
 	github.com/prometheus/procfs v0.12.0 // indirect
+	github.com/stretchr/objx v0.5.2 // indirect
 	golang.org/x/crypto v0.14.0 // indirect
 	golang.org/x/sys v0.16.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	google.golang.org/protobuf v1.32.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ product/test/product_repository_test.go
@@ -0,0 +1,69 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/rulanugrh/tokoku/product/internal/entity/domain"
+	"github.com/rulanugrh/tokoku/product/internal/mocks/repomocks"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/suite"
+)
+
+type ProductTest struct {
+	suite.Suite
+	repo repomocks.ProductInterface
+}
+
+func NewTestProduct() *ProductTest {
+	return &ProductTest{}
+}
+
+func (product *ProductTest) TestProductCreate() {
+	productResult := func (input domain.Product) *domain.Product  {
+		output := &domain.Product{}
+		output.Name = input.Name
+		output.Price = input.Price
+		output.CategoryID = input.CategoryID
+		return output
+	}
+
+	product.repo.On("Create", mock.MatchedBy(func (input domain.Product) bool {
+		return input.Name != "" && input.Price != 0 && input.QtyAvailable != 0
+	})).Return(productResult, nil)
+
+	data, err := product.repo.Create(domain.Product{
+		Name: "MacBook 15 Pro",
+		Description: "This is macbook pro",
+		Price: 15000000,
+		QtyAvailable: 10,
+		QtyOn: 0,
+		QtyReserved: 0,
+		CategoryID: 1,
+	})
+
+	product.Nil(nil, err)
+	product.Equal("MacBook 15 Pro", data.Name)
+	product.Equal(15000000, int(data.Price))
+	product.Equal(uint(1), data.CategoryID)
+}
+
+func (product *ProductTest) TestProductGetByID() {
+	productResult := func (id uint) *domain.Product  {
+		output := &domain.Product{}
+		output.ID = id
+		return output
+	}
+
+	product.repo.On("FindID", mock.MatchedBy(func (id uint) bool {
+		return true
+	})).Return(productResult, nil)
+
+	data, err := product.repo.FindID(uint(1))
+
+	product.Nil(nil, err)
+	product.Equal(uint(1), data.ID)
+}
+
+func TestProduct(t *testing.T) {
+	suite.Run(t, NewTestProduct())
+}
